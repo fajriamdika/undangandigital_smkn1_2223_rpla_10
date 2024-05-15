5e9ca244d77f66309ed534a7bc9009d3ca06b837
MushabAli <mushabalimaulana71@gmail.com>
2024-05-15 10:59:28 +0700

    tampilan paket

---
+++ resources/views/paket/tambah.blade.php
@@ -0,0 +1,12 @@
+<div class="row">
+    <div class="col-lg-12">
+        <div class="form-group">
+            <label>Nama Barang</label>
+            <input class="form-control" type="text" name="nama_barang" id="namaBarang"/>
+        </div>
+        <div class="form-group">
+            <label>Harga</label>
+            <input class="form-control" type="number" name="harga" id="hargaBarang"/>
+        </div>
+    </div>
+</div>
